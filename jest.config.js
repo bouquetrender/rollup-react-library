2dce2f93893e0134f217230d022f32d045cd71ad
Ve <bouquetrender@gmail.com>
2023-09-20 12:12:05 +0800

    update

--- jest.config.js
+++ jest.config.js
@@ -4,38 +4,38 @@
  */
 
 module.exports = {
-  // All imported modules in your tests should be mocked automatically
+  // 自动模拟所有导入的模块
   // automock: false,
 
-  // Stop running tests after `n` failures
+  // 在发生 `n` 个失败后停止运行测试
   // bail: 0,
 
-  // The directory where Jest should store its cached dependency information
+  // Jest 应该存储其缓存的依赖信息的目录
   // cacheDirectory: "/private/var/folders/7j/r_kgmlbj2512tj8c88xxg53r0000gn/T/jest_dx",
 
-  // Automatically clear mock calls, instances, contexts and results before every test
+  // 在每个测试之前自动清除模拟调用、实例、上下文和结果
   clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
+  // 指示是否在执行测试时应收集覆盖信息
   collectCoverage: true,
 
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // 一组用于收集覆盖信息的文件的 glob 模式数组
   collectCoverageFrom: ["src/**"],
 
-  // The directory where Jest should output its coverage files
+  // Jest 输出覆盖率文件的目录
   coverageDirectory: "coverage",
 
-  // An array of regexp pattern strings used to skip coverage collection
+  // 用于跳过覆盖率收集的正则表达式模式数组
   coveragePathIgnorePatterns: [
-     "/node_modules/",
-     "/stories/",
-     "^.*\.stories\.tsx?$"
+    "/node_modules/",
+    "/stories/",
+    "^.*\.stories\.tsx?$"
   ],
 
-  // Indicates which provider should be used to instrument code for coverage
+  // 指示用于覆盖率收集的提供程序
   // coverageProvider: "babel",
 
-  // A list of reporter names that Jest uses when writing coverage reports
+  // Jest 写覆盖率报告时使用的报告器名称列表
   // coverageReporters: [
   //   "json",
   //   "text",
@@ -43,45 +43,45 @@ module.exports = {
   //   "clover"
   // ],
 
-  // An object that configures minimum threshold enforcement for coverage results
+  // 配置覆盖率结果的最小阈值
   coverageThreshold: {
     "global": {
       "lines": 50
     }
   },
 
-  // A path to a custom dependency extractor
+  // 自定义依赖提取器的路径
   // dependencyExtractor: undefined,
 
-  // Make calling deprecated APIs throw helpful error messages
+  // 使调用弃用的 API 时抛出有用的错误消息
   // errorOnDeprecated: false,
 
-  // The default configuration for fake timers
+  // 自定义定时器的默认配置
   // fakeTimers: {
   //   "enableGlobally": false
   // },
 
-  // Force coverage collection from ignored files using an array of glob patterns
+  // 强制覆盖收集从被忽略的文件中匹配的内容
   // forceCoverageMatch: [],
 
-  // A path to a module which exports an async function that is triggered once before all test suites
+  // 触发所有测试套件之前执行一次的异步函数的模块路径
   // globalSetup: undefined,
 
-  // A path to a module which exports an async function that is triggered once after all test suites
+  // 触发所有测试套件之后执行一次的异步函数的模块路径
   // globalTeardown: undefined,
 
-  // A set of global variables that need to be available in all test environments
+  // 在所有测试环境中都需要可用的一组全局变量
   // globals: {},
 
-  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+  // 用于运行测试的最大工作进程数
   // maxWorkers: "50%",
 
-  // An array of directory names to be searched recursively up from the requiring module's location
+  // 递归搜索要求模块位置上方的一组目录名称
   // moduleDirectories: [
   //   "node_modules"
   // ],
 
-  // An array of file extensions your modules use
+  // 模块使用的文件扩展名数组
   // moduleFileExtensions: [
   //   "js",
   //   "mjs",
@@ -93,111 +93,113 @@ module.exports = {
   //   "node"
   // ],
 
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  // 从正则表达式到模块名称或模块名称数组的映射，允许使用单个模块来替代一组资源
   // moduleNameMapper: {},
 
-  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  // 在模块加载程序将模块视为“可见”之前，匹配所有模块路径的正则表达式模式数组
   // modulePathIgnorePatterns: [],
 
-  // Activates notifications for test results
+  // 激活测试结果的通知
   // notify: false,
 
-  // An enum that specifies notification mode. Requires { notify: true }
+  // 指定通知模式的枚举。要求 { notify: true }
   // notifyMode: "failure-change",
 
-  // A preset that is used as a base for Jest's configuration
+  // 用作 Jest 配置基础的预设
   // preset: undefined,
 
-  // Run tests from one or more projects
+  // 运行一个或多个项目的测试
   // projects: undefined,
 
-  // Use this configuration option to add custom reporters to Jest
+  // 使用此配置选项添加自定义报告器到 Jest
   // reporters: undefined,
 
-  // Automatically reset mock state before every test
+  // 在每个测试之前自动重置模拟状态
   // resetMocks: false,
 
-  // Reset the module registry before running each individual test
+  // 在运行每个单独的测试之前重置模块注册表
   // resetModules: false,
 
-  // A path to a custom resolver
+  // 自定义解析器的路径
   // resolver: undefined,
 
-  // Automatically restore mock state and implementation before every test
+  // 在每个测试之前自动恢复模拟状态和实现
   // restoreMocks: false,
 
-  // The root directory that Jest should scan for tests and modules within
+  // Jest 扫描测试和模块的根目录
   // rootDir: undefined,
 
-  // A list of paths to directories that Jest should use to search for files in
-   roots: [
-     "src"
-   ],
+  // 用于搜索文件的目录名称列表
+  roots: [
+    "src"
+  ],
 
-  // Allows you to use a custom runner instead of Jest's default test runner
+  // 允许您使用自定义运行器来替代 Jest 的默认测试运行器
   // runner: "jest-runner",
 
-  // The paths to modules that run some code to configure or set up the testing environment before each test
+  // 运行某些代码以在每个测试之前配置或设置测试环境的模块路径列表
   // setupFiles: [],
 
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  // 运行某些代码以在每个测试之前配置或设置测试框架的模块路径列表
   // setupFilesAfterEnv: [],
 
-  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  // 指定测试被视为缓慢并以此方式报告的秒数
   // slowTestThreshold: 5,
 
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+  // 用于快照测试的序列化器模块的路径列表
   // snapshotSerializers: [],
 
-  // The test environment that will be used for testing
+  // 用于测试的测试环境
   testEnvironment: "jsdom",
 
-  // Options that will be passed to the testEnvironment
+  // 传递给测试环境的选项
   // testEnvironmentOptions: {},
 
-  // Adds a location field to test results
+  // 在测试结果中添加位置字段
   // testLocationInResults: false,
 
-  // The glob patterns Jest uses to detect test files
+  // Jest 用于检测测试文件的 glob 模式
   // testMatch: [
   //   "**/__tests__/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
 
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  // 与所有测试路径匹配的正则表达式模式字符串数组，匹配的测试将被跳过
   // testPathIgnorePatterns: [
   //   "/node_modules/"
   // ],
 
-  // The regexp pattern or array of patterns that Jest uses to detect test files
+  // Jest 用于检测测试文件的正则表达式模式或模式数组
   // testRegex: [],
 
-  // This option allows the use of a custom results processor
+  // 此选项允许使用自定义结果处理器
   // testResultsProcessor: undefined,
 
-  // This option allows use of a custom test runner
+   // 允许使用自定义测试运行器
   // testRunner: "jest-circus/runner",
 
-  // A map from regular expressions to paths to transformers
+  // 从正则表达式到变换器路径的映射
   // transform: undefined,
 
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  // 与所有源文件路径匹配的正则表达式模式字符串数组，匹配的文件将跳过变换
   // transformIgnorePatterns: [
   //   "/node_modules/",
   //   "\\.pnp\\.[^\\/]+$"
   // ],
 
-  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+  // 与模块加载器自动返回模拟值之前匹配的正则表达式模式字符串数组
   // unmockedModulePathPatterns: undefined,
 
-  // Indicates whether each individual test should be reported during the run
+  // 指示是否应在运行期间报告每个单独的测试
   verbose: true,
 
-  // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
+  // 在监视模式下重新运行测试之前匹配所有源文件路径的正则表达式模式数组
   // watchPathIgnorePatterns: [],
 
-  // Whether to use watchman for file crawling
+  // 是否使用 watchman 进行文件遍历
   // watchman: true,
+
+  // 模块名称映射器，用于处理 CSS 文件
   moduleNameMapper: {
     ".(css|less|scss)$": "identity-obj-proxy",
   },
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-// rollup.config.js
 import terser from '@rollup/plugin-terser';
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
@@ -7,43 +6,38 @@ import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-
 import pkg from "./package.json" assert { type: 'json' };
 
 export default [
 	{
-		input: 'src/index.ts',
+		// 配置第一个输出的 bundle
+		input: 'src/index.ts', // 入口文件路径
 		output: [
 			{
-				file: pkg.main,
-				format: 'cjs',
-				sourcemap: true,
+				file: pkg.main, // CommonJS 格式的输出文件路径
+				format: 'cjs', // 输出格式为 CommonJS
+				sourcemap: true, // 生成 source map 文件
 			},
 			{
-				file: pkg.module,
-				format: "esm",
-				sourcemap: true,
+				file: pkg.module, // ES 模块格式的输出文件路径
+				format: "esm", // 输出格式为 ES 模块
+				sourcemap: true, // 生成 source map 文件
 			},
-			/* {
-				file: 'dist/bundle.min.js',
-				format: 'iife',
-				name: 'version',
-				plugins: [terser()]
-			} */
 		],
 		plugins: [
-			peerDepsExternal(),
-			resolve(),
-			commonjs(),
-			typescript({ tsconfig: "./tsconfig.json" }),
-			postcss(),
-			terser()
+			peerDepsExternal(), // 处理外部 peerDependencies
+			resolve(), // 解析模块路径
+			commonjs(), // 将 CommonJS 模块转换为 ES 模块
+			typescript({ tsconfig: "./tsconfig.json" }), // 处理 TypeScript 文件
+			postcss(), // 处理 CSS 文件
+			terser() // 压缩 JavaScript 代码
 		]
 	},
 	{
-		input: "dist/esm/types/index.d.ts",
-		output: [{ file: "dist/index.d.ts", format: "esm" }],
-		plugins: [dts()],
-		external: [/\.(css|less|scss)$/],
+		// 配置第二个输出，用于生成 TypeScript 类型定义文件
+		input: "dist/esm/types/index.d.ts", // 输入的 TypeScript 类型定义文件
+		output: [{ file: "dist/index.d.ts", format: "esm" }], // 输出的 ES 模块格式的类型定义文件
+		plugins: [dts()], // 处理类型定义文件
+		external: [/\.(css|less|scss)$/], // 外部模块，这里匹配 CSS 文件
 	},
 ];
